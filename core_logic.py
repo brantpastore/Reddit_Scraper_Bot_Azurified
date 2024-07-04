a362d33f99b47a6b1e29f1c6d771ef30037446c5
Brant Pastore <brant.pastore@gmail.com>
2024-07-04 11:30:21 -0500

    update core_logic to use autoinstaller

--- core_logic.py
+++ core_logic.py
@@ -54,6 +54,7 @@ class ScraperBot:
         options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         )
+        
         service = Service(chromedriver_autoinstaller.install())
         self.driver = webdriver.Chrome(service=service, options=options)
 
